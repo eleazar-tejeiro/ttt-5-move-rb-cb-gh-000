4017195d99d5f992689c2aa881ce49c43b9bb614
Uriel Eleazar Tejeiro Garcia <eleazartejeiro@gmail.com>
2019-03-21 04:05:58 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,9 @@
+# code your input_to_index and move method here!
+def input_to_index(input)
+  input.to_i
+  input = input - 1;
+end
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -5,5 +11,3 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
-# code your input_to_index and move method here!
